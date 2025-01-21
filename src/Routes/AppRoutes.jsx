a37b0c6157dcf0f57b8c2ac79cc7a6edca52d6e2
andreas <andr493i@elev.techcollege.dk>
2025-01-21 12:52:17 +0100

    added pages import
    add import to navbar.jsx and approutes.jsx

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,11 +1,19 @@
 import { Routes, Route} from "react-router-dom";
 
 import { HomePage } from "../Pages/HomePage/HomePage";
+import { PlakaterPage } from "../Pages/PlakaterPage/PlakaterPage";
+import { KontaktOsPage } from "../Pages/KontaktOsPage/KontaktOsPage";
+import { OmOsPage } from "../Pages/OmOsPage/OmOsPage";
+import { LoginPage } from "../Pages/LoginPage/LoginPage";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="PlakaterPage" element={ <PlakaterPage />} />
+      <Route path="KontaktOsPage" element={ <KontaktOsPage />} />
+      <Route path="OmOsPage" element={ <OmOsPage />} />
+      <Route path="LoginPage" element={ <LoginPage />} />
     </Routes>
   );
 }
