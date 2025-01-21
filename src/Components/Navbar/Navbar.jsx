cf0b84c795583338f1cdafb7d95000bba556f417
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-01-21 13:26:06 +0100

    Issue#3 static nav og lidt styling

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,24 @@
 import { NavbarStyled } from "./Navbar.Styled";
 import { NavLink } from "react-router-dom";
-import { HomePage } from "../../Pages/HomePage/HomePage";
-import { PlakaterPage } from "../../Pages/PlakaterPage/PlakaterPage";
-import { KontaktOsPage } from "../../Pages/KontaktOsPage/KontaktOsPage";
-import { OmOsPage } from "../../Pages/OmOsPage/OmOsPage";
-import { LoginPage } from "../../Pages/LoginPage/LoginPage";
 
 export const Navbar = () =>{
     return(
         <NavbarStyled>
             <ul>
                 <li>
-                <NavLink to={HomePage}>Forside</NavLink>
+                <NavLink to={"/"}>Forside</NavLink>
                 </li>
                 <li>
-                <NavLink to={PlakaterPage}>Plakater</NavLink>
+                <NavLink to={"PlakaterPage"}>Plakater</NavLink>
                 </li>
                 <li>
-                <NavLink to={OmOsPage}>Om os</NavLink>
+                <NavLink to={"OmOsPage"}>Om os</NavLink>
                 </li>
                 <li>
-                <NavLink to={KontaktOsPage}>Kontakt os</NavLink>
+                <NavLink to={"KontaktOsPage"}>Kontakt os</NavLink>
                 </li>
                 <li>
-                <NavLink to={LoginPage}>Login</NavLink>
+                <NavLink to={"LoginPage"}>Login</NavLink>
                 </li>
             </ul>
 
