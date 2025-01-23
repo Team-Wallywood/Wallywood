65fcf5a53ce531f488eff6a7cf732f64e29e6f6f
andreas <andr493i@elev.techcollege.dk>
2025-01-23 09:01:15 +0100

    page om os #16
    jeg har added star imged og added en h1 og en section til om os pagen

---
+++ src/Components/omos/omos.jsx
@@ -0,0 +1,9 @@
+export const Omos = () =>{
+    return(
+        <>
+        <h1>Om Os</h1>
+        <section>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ipsam iusto pariatur distinctio doloribus natus, sit voluptatem. Dolorem beatae eligendi saepe, ullam ipsam fuga assumenda aut commodi. Ipsam, exercitationem corporis.</section>
+        <img src="../../public/img/star.png" alt="" />
+        </>
+    )
+}
