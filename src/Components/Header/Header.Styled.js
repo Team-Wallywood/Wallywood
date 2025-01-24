2f5129aacb16c750dd6da33d2639d263b1d34993
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-01-24 09:46:24 +0100

    Jeg har tilføjet banner og styled

--- src/Components/Header/Header.Styled.js
+++ src/Components/Header/Header.Styled.js
@@ -6,4 +6,7 @@ export const HeaderStyled = styled.header`
   color: #D97852;
   font-size: 2rem;
   margin-left: 2rem;
+  img{
+    width: 100vw;
+  }
 `;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import { HeaderStyled } from "./Header.Styled";
-
+import  Banner  from "../../Assets/Images/curtain.jpg"
 
 
 export const Header = () =>{
     return(
         <HeaderStyled>
             <h1>Wallywood</h1>
+            <img src={Banner} alt="Banner" />
         </HeaderStyled>
     )
 }
