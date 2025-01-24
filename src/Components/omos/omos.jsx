263b4e250b47c2096954332255f19243b0823990
andreas <andr493i@elev.techcollege.dk>
2025-01-24 10:02:10 +0100

    #17 style om os
    hved ikke hvad eller skal gærer det vil ikke virke måske i kan prøve

--- src/Components/omos/omos.jsx
+++ src/Components/omos/omos.jsx
@@ -29,14 +29,12 @@ export const Omos = () =>{
         Quisque sit amet sem efficitur sapien bibendum mollis.
         Quisque in eros sit amet justo scelerisque rutrum.
         
-        
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Sed scelerisque ac ipsum sed faucibus. Vivamus mollis sagittis hendrerit.
         Donec turpis erat, vestibulum non turpis sed, finibus congue velit.
         Pellentesque sagittis est eget nisi malesuada, a aliquet est imperdiet.
         Aenean dapibus finibus laoreet. Pellentesque suscipit hendrerit enim, non tempor erat sodales quis.
         Nullam sed enim eu elit posuere commodo at în nisl. 
-        
         
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Sed scelerisque ac ipsum sed faucibus. Vivamus mollis sagittis hendrerit.
@@ -48,12 +46,10 @@ export const Omos = () =>{
         Phasellus dictum ligula rutrum neque pharetra, faucibus molestie libero ultricies.
         Aenean risus risus, sollicitudin at rutrum vel, venenatis ac urna. Phasellus a pulvinar massa.
         
-        
         Phasellus faucibus libero eu malesuada tristique. Donec tristique lacus in ipsum sollicitudin viverra.
         Sed porttitor sit amet felis accumsan egestas. Fusce quis commodo urna, non feugiat odio. Nam in tempus magna.
         Quisque eu neque sed lacus egestas pulvinar eget in lacus. Quisque sit amet sem efficitur sapien bibendum mollis.
         Quisque in eros sit amet justo scelerisque rutrum.
-        
         
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque ac ipsum sed faucibus.
         Vivamus mollis sagittis hendrerit. Donec turpis erat, vestibulum non turpis sed, finibus congue velit.
