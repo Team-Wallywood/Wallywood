6242496d63d2bda1aaf9b2652d1bcbde640eda4b
andreas <andr493i@elev.techcollege.dk>
2025-01-28 10:32:19 +0100

    #18 add kontakt os page
    added input navn. email og text med validering har også added 2 knapper en send og en annuller dog kan ikke se om det virker fordi at der er noget galt med suberbase siger den så ville spørger om en af jer kan se om det virker og vis ikke kan ratte det er ikke virker

---
+++ src/Components/KontaktOs/KontaktOs.Styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const KontaktOsStyled = styled.div`
+display: flex;
+flex-direction: column;
+gap: 10px;
+label{
+    display: flex;
+    flex-direction: column;
+}
+input{
+    width: 10rem;
+}
+
+button{
+    width: 10rem;
+}
+h1{
+    color: #D97852;
+}
+`;
---
+++ src/Components/KontaktOs/KontaktOs.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { KontaktOsStyled } from "./KontaktOs.Styled";
+
+export const Kontaktos = () => {
+  const [navn, setNavn] = useState("");
+  const [email, setEmail] = useState("");
+  const [text, setText] = useState("");
+  const [userError, setUserError] = useState("");
+
+  useEffect(() => {
+    if (navn.length > 0 && navn.length < 2) {
+      setUserError("Email must be at least 2 characters");
+    } else {
+      setUserError("");
+    }
+  }, [navn]);
+
+  useEffect(() => {
+    if (email.length > 0 && email.lenth > 5) {
+        setUserError("password must be at least 5 characters")
+    } else {
+        setUserError("");
+    }
+  }, [email]);
+
+  useEffect(() => {
+    if (text.length > 0 && text.lenth > 10) {
+        setUserError("password must be at least 10 characters")
+    } else {
+        setUserError("");
+    }
+  }, [text]);
+
+  return (
+    <>
+    <KontaktOsStyled>
+      <h1>Kontakt Os</h1>
+      <form>
+      <b>{userError}</b>
+        <label>
+          Navn:
+          <input type="navn" value={navn} onChange={(e) => setNavn(e.target.value)} />
+        </label>
+        <b>{userError}</b>
+        <label>
+          email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <b>{userError}</b>
+        <label>
+            Din besked:
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        </label>
+      </form>
+            <div>
+            <button>Send</button>
+            <button>anuller</button>
+            </div>
+    </KontakOsStyled>
+    </>
+)};
