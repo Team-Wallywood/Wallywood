d9e04e4b6be26185efebf50e274fc01d6ac81cfd
willy0483 <willyjensen251@gmail.com>
2025-01-30 10:55:55 +0100

    fix styled omos

--- src/Components/omos/omos.jsx
+++ src/Components/omos/omos.jsx
@@ -1,34 +1,54 @@
 import { OmosContainer } from "./omos.styled";
+import { Title } from "../Title/Title";
 
 export const Omos = () => {
-    return (
-        <>
-
-            <OmosContainer>
-                <div>
-                    <h1>Om Os</h1>
-                    <article>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ipsam iusto pariatur distinctio doloribus natus, sit voluptatem. Dolorem beatae eligendi saepe, ullam ipsam fuga assumenda aut commodi. Ipsam, exercitationem corporis.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur velit aliquam, nulla id molestias ex facere natus labore dolor eaque rem ea asperiores recusandae quidem neque. Vero dolore sunt corporis?
-                        <br />
-                        <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptatibus commodi alias, quae dolorem minima optio veritatis, distinctio quos eaque vero facilis. Sit aliquid a molestias aperiam cupiditate est consequatur.
-                        <br />
-                        <br />
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, qui omnis recusandae odio modi autem deleniti molestiae ut accusamus praesentium nam odit eos id facilis? Reprehenderit facilis facere eos? Doloremque.
-                        <br />
-                        <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex laborum quod, impedit repellat repellendus voluptas amet itaque sit adipisci cum inventore culpa, suscipit, dignissimos cupiditate error perspiciatis doloribus! Dignissimos, repellat?
-                        <br />
-                        <br />
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias earum voluptas iusto neque! Deleniti ab officiis pariatur nemo saepe dolore ipsam suscipit expedita ducimus laudantium, tenetur fugiat aspernatur, autem exercitationem!
-                        <br />
-                        <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum placeat dolore vel vitae cumque ratione? Assumenda, voluptatum inventore totam nostrum, molestiae rem laborum recusandae tempore id harum explicabo consequuntur tenetur!
-                    </article>
-                </div>
-                <img src="../../public/img/star.png" alt="" />
-            </OmosContainer>
-        </>
-    )
-}
+  return (
+    <>
+      <OmosContainer>
+        <Title>Om Os</Title>
+        <div>
+          <article>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+            ipsam iusto pariatur distinctio doloribus natus, sit voluptatem.
+            Dolorem beatae eligendi saepe, ullam ipsam fuga assumenda aut
+            commodi. Ipsam, exercitationem corporis. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Pariatur velit aliquam, nulla id
+            molestias ex facere natus labore dolor eaque rem ea asperiores
+            recusandae quidem neque. Vero dolore sunt corporis?
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+            voluptatibus commodi alias, quae dolorem minima optio veritatis,
+            distinctio quos eaque vero facilis. Sit aliquid a molestias aperiam
+            cupiditate est consequatur.
+            <br />
+            <br />
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
+            qui omnis recusandae odio modi autem deleniti molestiae ut accusamus
+            praesentium nam odit eos id facilis? Reprehenderit facilis facere
+            eos? Doloremque.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex laborum
+            quod, impedit repellat repellendus voluptas amet itaque sit adipisci
+            cum inventore culpa, suscipit, dignissimos cupiditate error
+            perspiciatis doloribus! Dignissimos, repellat?
+            <br />
+            <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+            earum voluptas iusto neque! Deleniti ab officiis pariatur nemo saepe
+            dolore ipsam suscipit expedita ducimus laudantium, tenetur fugiat
+            aspernatur, autem exercitationem!
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            placeat dolore vel vitae cumque ratione? Assumenda, voluptatum
+            inventore totam nostrum, molestiae rem laborum recusandae tempore id
+            harum explicabo consequuntur tenetur!
+          </article>
+          <img src="../../public/img/star.png" alt="" />
+        </div>
+      </OmosContainer>
+    </>
+  );
+};
--- src/Components/omos/omos.styled.js
+++ src/Components/omos/omos.styled.js
@@ -1,19 +1,21 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const OmosContainer = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-evenly;
-h1 {
-    color: #D97852;
-}
-article {
-    width: 50%;
-    padding: 10px;
-}
-img {
+  div {
+    display: grid;
+    grid-template-columns: 1.5fr 1fr;
+    margin: 0 2rem;
+  }
+  h1 {
+    color: #d97852;
+  }
+  article {
+    width: 100%;
+  }
+  img {
     display: block;
-    width: 25%;
-    height: 25%;
-}
+    width: 70%;
+    height: 80%;
+    margin: 0 auto;
+  }
 `;
